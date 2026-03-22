1ac8d029f1fce8ae392a8fcb09925188ac0e3cf8
Torben <tgotowik@protonmail.com.com>
2026-03-22 13:24:36 +0100

    add vault example

---
+++ ansible/inventory/group_vars/all/vault.ex
@@ -0,0 +1,19 @@
+---
+ansible_user: ""
+ansible_ssh_public_keys: 
+  - ""
+
+# webserver role
+str_webserver_ddclient_password: ""
+str_webserver_ddclient_login: ""
+
+# docker role
+str_docker_immich_db_password: ""
+
+# mail
+str_base_to_mail_address: ""
+str_from_mail_address: ""
+
+# smtp
+str_smtp_login: ""
+str_smtp_password: ""
